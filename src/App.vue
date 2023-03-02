8b56452c434d055c90d841ce44710d829f7622f4
Thanat Sukantatoon <thanatpp@odds.team>
2023-03-02 16:11:52 +0700

    fix: resolve import file

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-import { webview, callNative } from './webview'
+import { webview, callNative } from './webView'
 
 const preFetchResponse = ref({})
 
